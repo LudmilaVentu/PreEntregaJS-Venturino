45919ca2ff03bb7cb08114c9b1a9e9e2e481feca
Ludmila Venturino <ludmila.venturino@gmail.com>
2023-10-28 18:29:10 -0300

    Agrego script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,42 @@
-<script>
-var coder = "Se cambio"
-function change(){
-    document.getElementById("title").innerHTML =coder
-}
-</script>
+
+let nombre = prompt ("Cómo te llamas?")
+let confirmar = confirm("Confirmas que aceptas los terminos y condiciones?")
+alert("Bienvenido "+nombre+" a Entre Hilos")
+
+
+const carrito = [];
+
+console.log(carrito);
+
+const ingresarDatos = () => prompt('Ingrese el nombre del producto.');
+
+
+const agregarProducto = () => {
+	let confirmacion = confirm('Quieres agregar un producto?'),
+		producto;
+
+	while (confirmacion) {
+		producto = ingresarDatos();
+
+		console.log(producto);
+
+		if (!carrito.includes(producto) && producto !== null && producto !== '') {
+			carrito.push(producto);
+			alert("Se agrego un producto '", producto);
+			console.log(carrito);
+		} else {
+			alert("El producto ya se encuentra en el carrito '", producto, true);
+
+			console.log(carrito);
+		}
+
+
+		confirmacion = confirm('¿Quieres agregar un nuevo producto?');
+	}
+};
+
+agregarProducto();
+
+console.log(carrito.join(', '))
+alert(("Le agradecemos su compra. Usted va a comprar los siguientes productos: "+carrito.join(", ")))
+
